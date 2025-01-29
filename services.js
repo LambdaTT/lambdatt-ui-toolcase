1ef73ec1c5f40935ece5f464d917805e852997f0
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-01-29 18:54:14 -0300

    added event broadcaster service to the export

--- services.js
+++ services.js
@@ -3,5 +3,6 @@ import localData from './services/localData'
 import persistentNotifications from './services/persistentNotifications'
 import persistentRequests from './services/persistentRequests'
 import utils from './services/utils'
+import eventbroadcaster from './services/eventbroadcaster'
 
-export { http, localData, persistentNotifications, persistentRequests, utils };
+export { http, localData, persistentNotifications, persistentRequests, utils, eventbroadcaster };
--- services/eventbroadcaster.js
+++ services/eventbroadcaster.js
@@ -1,4 +1,4 @@
-export const EventBroadcaster = {
+export default {
   events: {},
 
   // Method to listen for an event
