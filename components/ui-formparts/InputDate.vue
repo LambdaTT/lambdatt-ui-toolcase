7a9d1e13e3913494fbde0a99e8a37fe4ecdc2287
Mkopaes <murilo.paes191@gmail.com>
2025-07-29 10:58:34 -0300

    Update: InputDate

--- components/ui-formparts/InputDate.vue
+++ components/ui-formparts/InputDate.vue
@@ -34,7 +34,7 @@ export default {
 
   computed: {
     hasError() {
-      return this.state === 'error' || this.error;
+      return this.state === 'error' || this.error || this.Error;
     }
   },
 
@@ -61,45 +61,11 @@ export default {
       this.error = false;
       this.errormsg = '';
       this.state = 'pending';
-      let emitValue = null;
 
       if (!val) return this.clear();
       else if (val.length < 10) return;
 
-      const [datePart, timePart] = val.split(' ');
-      const [day, month, year] = datePart.split('/').map(Number);
-
-      if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
-        this.error = true;
-        this.errormsg = 'Data inválida';
-        this.state = 'error';
-        return;
-      }
-
-      if (!!timePart) {
-        const timeParts = timePart.split(':');
-        if (timeParts.length < 2 || timeParts.length > 3) {
-          this.error = true;
-          this.errormsg = 'Hora inválida';
-          this.state = 'error';
-          return;
-        }
-        const [hours, minutes, seconds] = timeParts.map(Number);
-        if (!(hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && (seconds === undefined || (seconds >= 0 && seconds < 60)))) {
-          this.error = true;
-          this.errormsg = 'Hora inválida';
-          this.state = 'error';
-          return;
-        }
-      }
-
-      this.state = 'success';
-
-      emitValue = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-      if (this.withTime) emitValue += ` ${timePart ?? '00:00:00'}`;
-
-      this.$emit('update:modelValue', emitValue);
-      this.$emit('update:state', this.state);
+      this.emit(val);
     }
   },
 
@@ -108,6 +74,48 @@ export default {
       this.value = this.Default || null;
       this.error = false;
       this.errormsg = '';
+      this.emit(this.value);
+    },
+
+    emit(val) {
+      let emitValue = null;
+
+      if(!!val) {
+        const [datePart, timePart] = val.split(' ');
+        const [day, month, year] = datePart.split('/').map(Number);
+
+        if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
+          this.error = true;
+          this.errormsg = 'Data inválida';
+          this.state = 'error';
+          return;
+        }
+
+        if (!!timePart) {
+          const timeParts = timePart.split(':');
+          if (timeParts.length < 2 || timeParts.length > 3) {
+            this.error = true;
+            this.errormsg = 'Hora inválida';
+            this.state = 'error';
+            return;
+          }
+          const [hours, minutes, seconds] = timeParts.map(Number);
+          if (!(hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && (seconds === undefined || (seconds >= 0 && seconds < 60)))) {
+            this.error = true;
+            this.errormsg = 'Hora inválida';
+            this.state = 'error';
+            return;
+          }
+        }
+
+        this.state = 'success';
+
+        emitValue = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+        if (this.withTime) emitValue += ` ${timePart ?? '00:00:00'}`;
+      }
+
+      this.$emit('update:modelValue', emitValue);
+      this.$emit('update:state', this.state);
     }
   },
 
